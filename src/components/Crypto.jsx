5058d1e0bb6e81d1db318d5e416b6320eb9291ce
X-nimesh <nimush14@gmail.com>
2022-04-05 20:39:33 +0545

    publish in link

--- src/components/Crypto.jsx
+++ src/components/Crypto.jsx
@@ -1,38 +1,38 @@
-import React  from 'react'
-import millify from 'millify'
-import { Link } from 'react-router-dom'
-import { Col, Row,Card , Input} from 'antd'
+import React from "react";
+import millify from "millify";
+import { Link } from "react-router-dom";
+import { Col, Row, Card, Input } from "antd";
 
-import { useGetCrytosQuery } from '../services/cryptoApi'
-import { useState } from 'react'
-import { useEffect } from 'react'
+import { useGetCrytosQuery } from "../services/cryptoApi";
+import { useState } from "react";
+import { useEffect } from "react";
 
+const Crypto = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
 
-const Crypto = ({simplified}) =>{
+  const { data: cryptosList, isFetching } = useGetCrytosQuery(count);
+  const [cryptos, setCryptos] = useState(cryptosList?.data?.coins);
 
-  const count= simplified ? 10 : 100;
-
-  const { data:cryptosList, isFetching} = useGetCrytosQuery(count);
-  const[cryptos, setCryptos] = useState(cryptosList?.data?.coins);
-  
-  const[searchTerm, setSearchTerm] = useState('');
-  useEffect(()=>{
+  const [searchTerm, setSearchTerm] = useState("");
+  useEffect(() => {
     // setCryptos(cryptoList?.data?.coins);
-    const filteredData = cryptosList?.data?.coins?.filter((coin)=>coin.name.toLowerCase().includes(searchTerm.toLowerCase()));
+    const filteredData = cryptosList?.data?.coins?.filter((coin) =>
+      coin.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
     setCryptos(filteredData);
-  },[cryptosList,searchTerm])
-  
+  }, [cryptosList, searchTerm]);
+
   // useEffect(()=>{
   //   setCryptos(cryptoList?.data?.coins);
-    
+
   // },[cryptoList])
-  
+
   console.log(cryptos);
-  
-  if(isFetching) {
-    console.log('loading');
+
+  if (isFetching) {
+    console.log("loading");
     return (
-    <div class='containerL'>
+      <div class='containerL'>
         <div class='loader'>
           <div class='loader--dot'></div>
           <div class='loader--dot'></div>
@@ -42,43 +42,40 @@ const Crypto = ({simplified}) =>{
           <div class='loader--dot'></div>
           <div class='loader--text'></div>
         </div>
-    </div>
-  )
+      </div>
+    );
   }
-  
-
 
   return (
     <>
-    {!simplified &&(
-    <div className="search-crypto">
-      <Input placeholder='Seacrh Crypto'onChange={(e)=>setSearchTerm(e.target.value)}/>
-    </div>
-  )}
-      <Row gutter={[32,32]} className='crypto-card-container'>
+      {!simplified && (
+        <div className='search-crypto'>
+          <Input
+            placeholder='Seacrh Crypto'
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      )}
+      <Row gutter={[32, 32]} className='crypto-card-container'>
+        {cryptos?.map((currency, key) => (
+          <Col xs={24} sm={12} lg={6} className='crypto-card' key={key}>
+            <Link to={`/crypto/${currency.id}`} />
+            <Card
+              title={`${currency.rank}. ${currency.name}`}
+              extra={<img className='crypto-image' src={currency.iconUrl} />}
+              hoverable
+            >
+              <p>Price: ${millify(currency.price)}</p>
 
-        {cryptos?.map((currency,key) => (
-            <Col xs={24} sm={12} lg={6} className='crypto-card' key={key}>
-            
-               <Link to={`/crypto/${currency.id}`}/>
-              <Card 
-                  title={`${currency.rank}. ${currency.name}`}
-                  extra={<img className='crypto-image' src={currency.iconUrl} />}
-                  hoverable
-                  >
-                    <p>Price: ${millify(currency.price)}</p>
-                    
-                    <p>Market Cap: {millify(currency.marketCap)}</p>
-                    <p>Daily Change: {millify(currency.change)}%</p>
-                   
-                </Card> 
-              </Col>
-        ))
-        }
+              <p>Market Cap: {millify(currency.marketCap)}</p>
+              <p>Daily Change: {millify(currency.change)}%</p>
+            </Card>
+          </Col>
+        ))}
       </Row>
     </>
-  )
-      // }
-}
+  );
+  // }
+};
 
-export default Crypto
+export default Crypto;
